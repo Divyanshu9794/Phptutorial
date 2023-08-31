aadb96d5206dd652bc077aff68cb79b69e9b095e
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-08-31 18:51:28 +0530

    .

---
+++ constant.php
@@ -0,0 +1,14 @@
+<?php
+
+// we cannot reassign value or redefine vaariable which is of constant type
+
+define("test",50);
+// define("test",10);
+
+echo test;
+
+// $sum = TEST +20;
+// echo $sum;
+
+
+?>
