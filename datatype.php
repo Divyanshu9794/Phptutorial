770eb8c1451e2f50495adcc8c43ed816d2c8994d
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-08-31 17:48:14 +0530

    .

---
+++ datatype.php
@@ -0,0 +1,23 @@
+<?php
+
+$x = "Divyanshu Singh";
+
+echo $x . "<br>";
+var_dump($x);
+// this will return the datatype of the variable and number of character in that variable 
+
+
+$y = false;
+
+echo $y . "<br>";
+var_dump($y);
+
+$z = array("html","css","js");
+echo $z[1] . "<br>";
+var_dump($z);
+
+
+$a = null;
+echo $a . "<br>";
+var_dump($a);
+?>
