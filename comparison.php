1f0270274cf903163ac344dfcde8147fa9387e80
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-08-31 19:03:57 +0530

    .

---
+++ comparison.php
@@ -0,0 +1,19 @@
+<?php
+$a = 40;
+$b=30;
+// echo $a == $b;
+// this will return true since value is same 
+
+
+// echo  $a < $b;
+//return 1 as true
+// return nothing when false;
+
+
+// Spaceship operator
+
+
+echo $a <=> $b;
+
+
+?>
