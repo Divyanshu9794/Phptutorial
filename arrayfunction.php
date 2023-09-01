8998c00cfb554443a33cfc7dc459d7d1e874a428
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-01 15:24:03 +0530

    .

---
+++ arrayfunction.php
@@ -0,0 +1,19 @@
+<?php
+$food = array('orange','orange','mango','banana');
+echo "<pre>";
+print_r(array_count_values($food));
+echo "</pre>";
+// for($i=0;$i<count($food);$i++){
+//     echo $food[$i] . " <br>";
+// }
+// $emp = [
+//     [1,"aman","manager",50000],
+//     [2,"amir","CO",5000],
+//     [3,"salman","PO",150000],
+//     [4,"sahrukh","driver",500]
+
+// ];
+
+// echo count($emp,1);
+
+?>
