46ab8a0a10ce651e04093e47a8809a7773fe5292
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-01 10:35:36 +0530

    functuion

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+function hello(){
+    echo "Hello Divyanshu<br>";
+}
+function Hi(){
+    echo "HI Divyanshu<br>";
+}
+hello();
+hello();
+hello();
+echo "This is an example out of the function";
+hello();
+hello();
+hello();
+hello();
+hello();
+hi();
+
+?>
