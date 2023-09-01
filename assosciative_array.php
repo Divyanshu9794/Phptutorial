ad6ac3ee4b9bcb226480e259762d51e7f54bc8a7
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-01 13:06:17 +0530

    .

---
+++ assosciative_array.php
@@ -0,0 +1,22 @@
+<?php
+
+$a = array(
+    "bill" => "25",
+    "aman" => 2.222222,
+    "khan" =>20
+);
+// echo "<pre>";
+// print_r($a);
+// echo "</pre>";
+
+echo "<pre>";
+var_dump($a);
+echo "</pre>";
+
+
+echo $a["khan"] . "<br>";
+echo $a["aman"] . "<br>";
+echo $a["bill"] . "<br>";
+
+
+?>
