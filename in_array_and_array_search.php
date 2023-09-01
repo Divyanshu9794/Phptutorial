d292ffdc090d23ba7669104f68bcc3534a627edb
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-01 16:17:20 +0530

    .

---
+++ in_array_and_array_search.php
@@ -0,0 +1,17 @@
+<?php
+
+$food = array('orange','pineapple','mango','banana','55');
+
+echo array_search('banana',$food);
+
+
+// echo in_array('orange',$food);
+// if(in_array(55,$food,true)){
+//     echo "Founded Successfully";
+// }
+// else{
+//     echo "Cannot Be found";
+// }
+
+
+?>
