406f2593afb2cb3e913a54d06fb9ae61dee60a16
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-01 10:31:06 +0530

    goto

---
+++ goto.php
@@ -0,0 +1,13 @@
+<?php
+echo "Hello <br>";
+echo "Hello <br>";
+goto abc; // redirect to label abc
+echo "Hello <br>";
+echo "Hello <br>";
+echo "Hello <br>";
+echo "Hello <br>";
+abc: //label declaration 
+echo "Divyanshu this is label abc";
+
+
+?>
