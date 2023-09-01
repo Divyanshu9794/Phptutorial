ea932c49347b50a9c3345cd9c90a79fd03ee0439
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-01 12:53:28 +0530

    .

---
+++ array.php
@@ -0,0 +1,22 @@
+<?php
+$color = array("red","yellow","blue","green");
+
+echo "<ul>";
+for($i =0 ;$i<4;$i++)
+{
+    echo "<li>$color[$i] . </li>";
+}
+echo "</ul>";
+
+// $color[0]="red";
+// $color[1]="blue";
+// $color[0]="red";
+// $color[0]="red";
+
+
+// echo $color[0];
+// echo "<pre>";
+// print_r($color);
+
+// echo "</pre>";
+?>
