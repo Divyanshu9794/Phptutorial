77c13264d07697bf535b2a11af3bf4f646c2a4f0
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-01 10:43:03 +0530

    .

---
+++ functionwithparameter.php
@@ -0,0 +1,19 @@
+<?php
+function hello($name="Aman",$relation="Bhai"){
+    echo "Hello $name. You are my .$relation <br>";
+}
+hello("divyanshu");
+
+
+function sum($a,$b){
+    echo $a+$b;
+}
+
+$s =1;
+$x =22;
+sum($s,$x);
+
+
+
+
+?>
