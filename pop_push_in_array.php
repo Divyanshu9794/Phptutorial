e2ddd859d78ce30b1ebd10c466de1e5b4bc83016
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-02 13:44:34 +0530

    .

---
+++ pop_push_in_array.php
@@ -0,0 +1,16 @@
+<?php
+
+$fruits = ['orange','banana','apple','grapes'];
+
+// array_pop($fruits);
+// array_push($fruits,"papaya");
+array_push($fruits,"papaya","Cheeku");
+
+echo "<pre>";
+
+print_r($fruits);
+echo "</pre>";
+
+
+
+?>
