a94b24c1a83663c7e8fbc60ba149ea97e061682a
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-02 14:20:02 +0530

    .

---
+++ array_shift.php
@@ -0,0 +1,16 @@
+<?php
+
+$fruits = ['orange','banana','apple','grapes'];
+
+// array_shift($fruits);
+
+array_unshift($fruits,"seb");
+
+echo "<pre>";
+print_r($fruits);
+echo "</pre>";
+
+
+
+
+?>
