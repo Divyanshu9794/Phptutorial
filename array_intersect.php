7e1a5009626963d1d53ba8cd77d459b8db39645a
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-03 12:12:58 +0530

    .

--- array_intersect.php
+++ array_intersect.php
@@ -10,8 +10,10 @@ $a2 = ["a"=>"red","f"=>"green","d"=>"purple"];
 // $newarray = array_intersect_key($a1,$a2);
 //it intersect array by key
 
-$newarray = array_intersect_assoc($a1,$a2);
+// $newarray = array_intersect_assoc($a1,$a2);
 //it intersect array by key and value both 
+$newarray = array_intersect_uassoc($a1,$a2,"compare");
+// this function will take an additional parameter i.e, a user defined function
 
 echo "<pre>";
 print_r($newarray);
