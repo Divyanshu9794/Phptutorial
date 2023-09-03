3fa95a40994944f9e432be76cffbda0ab1b2fb38
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-03 12:22:25 +0530

    .

--- array_diff.php
+++ array_diff.php
@@ -4,5 +4,9 @@ $a1 = array("a"=>"red",
 "b"=>"green","c"=>"blue","d"=>"yellow");
 $a2 = array("a"=>"red","f"=>"green","d"=>"purple");
 
+array_diff($a1,$a2);
+echo "<pre>";
+print_r($newarray);
+echo "</pre>";
 
 ?>
