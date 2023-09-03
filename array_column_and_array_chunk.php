28d2f84684199da33cf41b1462b23260787d72c7
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-03 12:39:16 +0530

    .

---
+++ array_column_and_array_chunk.php
@@ -0,0 +1,22 @@
+<?php
+$array = array(
+    array(
+        'id'=>2201,
+        'first_name'=>'Anil',
+        'last_name' => 'Kapoor',
+    ),
+    array(
+        'id'=>2202,
+        'first_name'=>'Salman',
+        'last_name' => 'Khan',
+    ),
+    array(
+        'id'=>2203,
+        'first_name'=>'John',
+        'last_name' => 'Abraham',
+    ),
+)
+
+
+
+?>
