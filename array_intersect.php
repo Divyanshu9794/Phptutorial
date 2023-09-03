5a29e978f52e484af58979382cc5f5d9728c6d10
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-03 11:59:11 +0530

    .

--- array_intersect.php
+++ array_intersect.php
@@ -4,8 +4,15 @@ $a1 = ["a"=>"red",
 "b"=>"green","c"=>"blue","d"=>"yellow"];
 
 $a2 = ["a"=>"red","f"=>"green","d"=>"purple"];
-$newarray = array_intersect($a1,$a2);
+// $newarray = array_intersect($a1,$a2);
+
 //it intersect array by value not by key
+// $newarray = array_intersect_key($a1,$a2);
+//it intersect array by key
+
+$newarray = array_intersect_assoc($a1,$a2);
+//it intersect array by key and value both 
+
 echo "<pre>";
 print_r($newarray);
 echo "</pre>";
