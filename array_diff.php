42f16c06143cf38dc3a6a0008d1429a7c47dd360
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-03 12:29:18 +0530

    .

--- array_diff.php
+++ array_diff.php
@@ -1,5 +1,10 @@
 <?php
-
+function compare($a,$b){
+    if($a===$b){
+        return 0;
+    }
+    return ($a>$b)?1:-1;
+}
 $a1 = array("a"=>"red",
 "b"=>"green","c"=>"blue","d"=>"yellow");
 $a2 = array("a"=>"red","f"=>"green","d"=>"purple");
@@ -12,7 +17,7 @@ $a2 = array("a"=>"red","f"=>"green","d"=>"purple");
 //it will take difference of only keys  and return the array elements which are not common in both the array
 // $newarray= array_diff_assoc($a1,$a2);
 //array diff function is case sensitive it will treat two keys different if one is in capital and other is smaalll
-$newarray= array_diff_uassoc($a1,$a2);
+$newarray= array_diff_uassoc($a1,$a2,"compare");
 
 
 
