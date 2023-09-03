ac33a16ddb878f9f4790680d7eae85aa77949029
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-03 12:27:50 +0530

    .

--- array_diff.php
+++ array_diff.php
@@ -8,8 +8,11 @@ $a2 = array("a"=>"red","f"=>"green","d"=>"purple");
 //it will take difference of only values and return the array elements which are not common in both the array
 
 
-$newarray= array_diff_key($a1,$a2);
+// $newarray= array_diff_key($a1,$a2);
 //it will take difference of only keys  and return the array elements which are not common in both the array
+$newarray= array_diff_assoc($a1,$a2);
+//array diff function is case sensitive it will treat two keys different if one is in capital and other is smaalll
+
 
 echo "<pre>";
 print_r($newarray);
