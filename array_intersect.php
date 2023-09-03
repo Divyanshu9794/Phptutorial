9ac5b3449240a032e04f71bf7de5b2bdede50407
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-03 11:56:52 +0530

    .

--- array_intersect.php
+++ array_intersect.php
@@ -4,6 +4,9 @@ $a1 = ["a"=>"red",
 "b"=>"green","c"=>"blue","d"=>"yellow"];
 
 $a2 = ["a"=>"red","f"=>"green","d"=>"purple"];
-
+$newarray = array_intersect($a1,$a2);
+echo "<pre>";
+print_r($newarray);
+echo "</pre>";
 
 ?>
