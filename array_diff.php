ed3f695faccba7dc8bae45807effccfd5aac12da
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-03 12:25:55 +0530

    .

--- array_diff.php
+++ array_diff.php
@@ -4,7 +4,13 @@ $a1 = array("a"=>"red",
 "b"=>"green","c"=>"blue","d"=>"yellow");
 $a2 = array("a"=>"red","f"=>"green","d"=>"purple");
 
-$newarray= array_diff($a1,$a2);
+// $newarray= array_diff($a1,$a2);
+//it will take difference of only values and return the array elements which are not common in both the array
+
+
+$newarray= array_diff_key($a1,$a2);
+//it will take difference of only keys  and return the array elements which are not common in both the array
+
 echo "<pre>";
 print_r($newarray);
 echo "</pre>";
