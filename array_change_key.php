abbaf17411af6dc5fd254caa70ec722a4db15741
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-05 16:55:45 +0530

    .

--- array_change_key.php
+++ array_change_key.php
@@ -1,11 +1,13 @@
 <?php
 $a = array(
-    "Bill" =>10,
+    "BILL" =>10,
     "Joe" =>20,
     "peter" =>30
 );
 // $newarray = array_flip($a);//it will flip array key with value
-$newarray = array_change_key_case($a);
+// $newarray = array_change_key_case($a,CASE_UPPER);
+//convert the key in upper case
+$newarray = array_change_key_case($a,CASE_LOWER);
 echo "<pre>";
 print_r($newarray);
 echo "</pre>"; 
