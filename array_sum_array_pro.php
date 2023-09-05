f0069d5c50991af2a303ba11fd39cc78de9628cb
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-05 18:59:23 +0530

    .

--- array_sum_array_pro.php
+++ array_sum_array_pro.php
@@ -3,6 +3,6 @@
 // echo "Sum = " . array_sum($a); 
 //it will add all the values of the arrays
 $a = [1,2,3,4];
-echo "Prodict = " . array_product($a); 
+echo "Product = " . array_product($a); 
 //it will multiply all the values of the arrays
 ?>
