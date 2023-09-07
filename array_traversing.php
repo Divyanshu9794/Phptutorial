b1537d0a3c9242f99a5a5cfcf72f4bea1af66196
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-07 16:48:58 +0530

    .

--- array_traversing.php
+++ array_traversing.php
@@ -8,7 +8,12 @@ $food = array('orange',
 //print the value present at initial index
 
 
-echo "<b> Current :</b>" . current($food) . "<br>";
-echo "<b>Key: </b>" .key($food);
+// echo "<b> Current :</b>" . current($food) . "<br>";
+// echo "<b>Key: </b>" .key($food);
 
+
+echo "<b> Current :</b>" . current($food) . "<br>";
+echo "<b>Key: </b>" .key($food) . "<br>";
+echo "<b>Pos: </b>" .pos($food);
+//return the value of the element which is at current position
 ?>
