c5c92d37ca8907b2037477437d0ab848518c9826
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-07 09:53:32 +0530

    .

--- array_walk.php
+++ array_walk.php
@@ -5,9 +5,10 @@ $fruits = array(
     "c"=>"Banana",
     "d"=>"Apple"
 );
-array_walk($fruits,"myfunction");
-function myfunction($value,$key){
-    echo "$key : $value<br>";
+// array_walk($fruits,"myfunction","is a key of");
+
+function myfunction($value,$key,$param){
+    echo "$key $param  $value<br>";
 }
 
 
