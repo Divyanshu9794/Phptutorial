a84ddd5cff6fac80fa9d99374212d5d60f644709
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-07 16:32:18 +0530

    .

--- array_sorting.php
+++ array_sorting.php
@@ -11,18 +11,48 @@
 
 // sort($a);
 
-$food = array(
-    "d"=>"lemon",
-    "a"=>"orange",
-    "b"=>"banana",
-    "c"=>"apple"
-);
+// $food = array(
+//     "d"=>"lemon",
+//     "a"=>"orange",
+//     "b"=>"banana",
+//     "c"=>"apple"
+// );
 // asort($food);
 // //sort the associative array according to value
 // arsort($food);
 //sort the associative array in descending order  according to value
 
 
+// ksort($food);
+//sort the associative array according to there keys 
+// krsort($food);
+//sort the associative array in descending order according to there keys 
+
+
+
+// $a = array(
+//     "img12.png","img10.png","img2.png","img1.png"
+// );
+// natsort($a);
+//sort the array according to value but consider all extension and name and number too....
+$a = array(
+    "Img12.png","img10.png","img2.png","img1.png"
+);
+
+// natcasesort($a);
+//sort same as natsort but it is case sensitive
+$food = array(
+    "lemon",
+    "orange",
+    "banana",
+    "apple"
+);
+array_multisort($a,$food);
+//this will sort all the value present in both the array but there is one constraint that both the arrays must be of same size
+
+echo "<pre>";
+print_r($a);
+echo "</pre>";
 
 echo "<pre>";
 print_r($food);
