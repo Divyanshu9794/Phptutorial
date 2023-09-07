cffe575bade78f69ff97a69d390bd89d54b017c4
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-07 09:44:11 +0530

    .

--- array_rand.php
+++ array_rand.php
@@ -1,4 +1,20 @@
 <?php
+ $color = ["red","blue","yellow","orange"];
+
+ $newarray = array_rand($color,2);
+ //it will return any random value
+shuffle($color);
+//it will shuffle the values that are present in the array
+
+echo "<pre>";
+print_r($color);
+echo "</pre>";
+
+// // echo $color[$newarray];
+// echo $color[$newarray[0]] . "<br>";
+// echo $color[$newarray[1]];
+
+
 
 
 
