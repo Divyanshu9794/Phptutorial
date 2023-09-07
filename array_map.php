7f330487eefe293e7c756c770807711edecfa4e4
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-07 11:21:25 +0530

    .

--- array_map.php
+++ array_map.php
@@ -1,13 +1,25 @@
 <?php
-function square($n,$m){
-    return [$n =>  $m];
-}//now this will create a multidimensional
-$a = [1,2,3,4,5];
-$b = ['lemon','orange','banana','apple','guava'];
+// function square($n,$m){
+//     return [$n =>  $m];
+// }//now this will create a multidimensional
+// $a = [1,2,3,4,5];
+// $b = ['lemon','orange','banana','apple','guava'];
 // $newarray =array_map('square',$a,$b);
 //it will map all the index of the array with the output of the function here we are mapping each index with the square of the value starting from 1
-$newarray =array_map(null,$a,$b);
+// $newarray =array_map(null,$a,$b);
 //this will create a 2d array picking up 1 value from array 1 and other value from array 2
+function square($n){
+    return strtoupper($n);
+}
+//now this will convert all array values into upper case letter
+
+
+
+$a = array("one"=>"Apple",
+    "two" =>"Banana",
+    "three" => "Orange"
+);
+$newarray =array_map('square',$a);
 echo "<pre>";
 print_r($newarray);
 echo "</pre>";
