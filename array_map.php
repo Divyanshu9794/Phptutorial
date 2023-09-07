8e56f596cb8726f4ca9e2ea06ac0d7abb145ee4b
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-07 11:13:23 +0530

    .

--- array_map.php
+++ array_map.php
@@ -1,10 +1,13 @@
 <?php
-function square($n){
-    return $n*$n;
-}
+function square($n,$m){
+    return [$n =>  $m];
+}//now this will create a multidimensional
 $a = [1,2,3,4,5];
-$newarray =array_map('square',$a);
+$b = ['lemon','orange','banana','apple','guava'];
+// $newarray =array_map('square',$a,$b);
 //it will map all the index of the array with the output of the function here we are mapping each index with the square of the value starting from 1
+$newarray =array_map(null,$a,$b);
+//this will create a 2d array picking up 1 value from array 1 and other value from array 2
 echo "<pre>";
 print_r($newarray);
 echo "</pre>";
