be770c229537863cf26a91a4cd562e88124a043d
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-07 16:24:17 +0530

    .

---
+++ array_sorting.php
@@ -0,0 +1,31 @@
+<?php
+
+// $a = ['orange','apple','banana ','grapes','mango'];
+// sort($a);
+//it will sort the value of array in ascending order 
+// rsort($a);
+//sort the value of array in descending order
+
+
+// $a =[1,2,3,4,59,5,8,5,56,59];
+
+// sort($a);
+
+$food = array(
+    "d"=>"lemon",
+    "a"=>"orange",
+    "b"=>"banana",
+    "c"=>"apple"
+);
+// asort($food);
+// //sort the associative array according to value
+// arsort($food);
+//sort the associative array in descending order  according to value
+
+
+
+echo "<pre>";
+print_r($food);
+echo "</pre>";
+
+?>
