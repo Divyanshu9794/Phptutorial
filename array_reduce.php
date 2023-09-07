63f355dc5e997eac52cba09885ebcd0b6f0914e4
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-07 16:17:22 +0530

    .

--- array_reduce.php
+++ array_reduce.php
@@ -1,14 +1,17 @@
 <?php
 function myfunction($n,$m){
-    return $n . "-" . $m;
+    return $n + $m;
 }
 
-$a = ['orange','banana','apple'];
+// $a = ['orange','banana','apple'];
+
 
 // $newarray = array_reduce($a,'myfunction');
 //it will concatenate all the values that are present in the array 
-$newarray = array_reduce($a,'myfunction','lemon');
-//it will concatenate all the values that are present in the array with the starting value lemon 
+// $newarray = array_reduce($a,'myfunction','lemon');
+//it will concatenate all the values that are present in the array with the starting value lemon (we can also take numeric value)
+$a = [1,2,3,4,5];
+$newarray = array_reduce($a,'myfunction');
 
 echo "<pre>";
 print_r($newarray);
