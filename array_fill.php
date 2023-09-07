f61cf1ed80e78e83654a622f9ab0e23a83dd1c51
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-07 09:48:48 +0530

    .

--- array_fill.php
+++ array_fill.php
@@ -1,5 +1,14 @@
 <?php
 
+$a = ["a","b","c","d","e"];
+// $newarray = array_fill_keys($a,"Hello");
+//it will provide value to index array and make it a associatibe array
+$newarray = array_fill(0,8,"Hello");
+//it will array by value from starting index to the number of values
 
+
+echo "<pre>";
+print_r($newarray);
+echo "</pre>";
 
 ?>
