bcd8406255c555f0424cbdb216b7e9a990dea1d9
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-07 16:14:13 +0530

    .

--- array_reduce.php
+++ array_reduce.php
@@ -5,8 +5,11 @@ function myfunction($n,$m){
 
 $a = ['orange','banana','apple'];
 
-$newarray = array_reduce($a,'myfunction');
+// $newarray = array_reduce($a,'myfunction');
 //it will concatenate all the values that are present in the array 
+$newarray = array_reduce($a,'myfunction','lemon');
+//it will concatenate all the values that are present in the array with the starting value lemon 
+
 echo "<pre>";
 print_r($newarray);
 echo "</pre>";
