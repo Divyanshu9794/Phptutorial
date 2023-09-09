a8776cfa66f15cce13e7dbaf473d7d88db8703cb
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-09 10:38:10 +0530

    .

---
+++ array_range.php
@@ -0,0 +1,27 @@
+<?php
+$newarray = range(1,100,20);
+//ye function new array naam ka ek array bnayega aur usme saare element jo uss range me h unhe array element me insert kr dega jaise isme humne range diya h 1 s 10 to ye saare element 1 ->10 tak ko  store kr dega 
+//3rd value jo 20 h wo step btayega 
+//output;
+
+// Array
+// (
+//     [0] => 1
+//     [1] => 2
+//     [2] => 3
+//     [3] => 4
+//     [4] => 5
+//     [5] => 6
+//     [6] => 7
+//     [7] => 8
+//     [8] => 9
+//     [9] => 10
+// )
+
+
+
+echo "<pre>";
+print_r($newarray);
+echo "</pre>";
+
+?>
