fad9fc324de5125ca4388fac12b63ec3a5b3a5ba
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-09 10:11:47 +0530

    .

---
+++ array_list.php
@@ -0,0 +1,22 @@
+<?php
+// $color = array('red','green','blue');
+// $color = array(1,2,3,6,4,8,6,9);
+
+$color = array(
+    0=>'red',
+    1=>'green',
+    2=>'blue'
+);
+
+// list($a,$b,$c)= $color;
+list($a[0],$a[1],$a[2])= $color;
+
+
+echo "Value of a: $a[0] <br>";
+echo "Value of b: $a[1] <br>";
+
+echo "Value of c: $a[2] <br>";
+
+
+
+?>
